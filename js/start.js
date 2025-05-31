e79658805da9414f7cbe554d2ac3bd1928559a94
Your Name <you@example.com>
2025-05-31 16:14:58 -0400

    add viewport

--- js/start.js
+++ js/start.js
@@ -1,5 +1,11 @@
 window.MobileApi = {
     start: function (root) {
+        const viewport = new qx.ui.mobile.container.Composite;
+        viewport._setStyle('backgroundColor', 'red');
+        viewport._setStyle('height', '100%');
+        root.add(viewport);
+
         console.log('MobileApi START2', root);
+        window.ROOT = root;
     }
 };
