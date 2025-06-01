d5d81871b1a54bc7999347c50bfb747397c8d3fe
Your Name <you@example.com>
2025-05-31 20:40:53 -0400

    start.js

--- js/start.js
+++ js/start.js
@@ -1,11 +1,13 @@
+function createViewport(root) {
+    const viewport = new qx.ui.mobile.container.Composite;
+    viewport._setStyle('backgroundColor', 'lightgrey');
+    viewport._setStyle('height', '100%');
+    viewport.setLayout(new qx.ui.layout.VBox);
+    root.add(viewport);
+    return viewport;
+}
 window.MobileApi = {
     start: function (root) {
-        const viewport = new qx.ui.mobile.container.Composite;
-        viewport._setStyle('backgroundColor', 'red');
-        viewport._setStyle('height', '100%');
-        root.add(viewport);
-
-        console.log('MobileApi START2', root);
-        window.ROOT = root;
+        const viewport = createViewport(root);
     }
 };
