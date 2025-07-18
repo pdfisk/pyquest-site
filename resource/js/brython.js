13f90ed49352471abafed170399a48dbcdb7c763
Your Name <you@example.com>
2025-07-18 09:55:19 -0400

    version

--- resource/js/brython.js
+++ resource/js/brython.js
@@ -213,7 +213,7 @@ $B.unicode_bidi_whitespace=[9,10,11,12,13,28,29,30,31,32,133,5760,8192,8193,8194
 ;
 __BRYTHON__.implementation=[3,13,2,'dev',0]
 __BRYTHON__.version_info=[3,13,0,'final',0]
-__BRYTHON__.compiled_date="2025-07-17T14:18:52Z"
+__BRYTHON__.compiled_date="2025-07-18T13:47:59Z"
 __BRYTHON__.timestamp=1752757321
 __BRYTHON__.builtin_module_names=["_ajax","_ast","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_random","_sre","_sre_utils","_string","_strptime","_svg","_symtable","_tokenize","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","encoding_cp932","hashlib","html_parser","marshal","math","modulefinder","posix","pyexpat","python_re","unicodedata","xml_helpers","xml_parser"];
 ;
@@ -28886,11 +28886,7 @@ throw Error(`unknown parse mode: ${p.mode}`)}};
 console.log('eval_code',code);},get_timestamp:()=>{
 return $B.compiled_date;}};})(__BRYTHON__);
 ;
-(function($B){const url='resource/python/index.py';
-fetch(url)
-.then(x=> x.text())
-.then(src=>{
-console.log('SRC',src);});})(__BRYTHON__);
+
 ;
 (function($B){$B.whenReady=new Promise(function(resolve,reject){resolve()})})(__BRYTHON__);
 ;
