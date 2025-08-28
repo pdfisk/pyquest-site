032d40dd2c43e04b5149d3b62b264678a1281de5
Peter Fisk <peter.fisk@gmail.com>
2025-08-28 15:34:22 -0400

    brython version

--- js/brython.js
+++ js/brython.js
@@ -222,8 +222,8 @@ $B.unicode_bidi_whitespace=[9,10,11,12,13,28,29,30,31,32,133,5760,8192,8193,8194
 ;
 __BRYTHON__.implementation=[3,13,2,'dev',0]
 __BRYTHON__.version_info=[3,13,0,'final',0]
-__BRYTHON__.compiled_date="2025-08-25 11:04:10.629124"
-__BRYTHON__.timestamp=1756134250628
+__BRYTHON__.compiled_date="2025-08-28 15:33:11.505836"
+__BRYTHON__.timestamp=1756409591503
 __BRYTHON__.builtin_module_names=["_ajax","_ast","_base64","_binascii","_io_classes","_json","_jsre","_locale","_multiprocessing","_posixsubprocess","_profile","_random","_sre","_sre_utils","_string","_strptime","_svg","_symtable","_tokenize","_webcomponent","_webworker","_zlib_utils","array","builtins","dis","encoding_cp932","hashlib","html_parser","marshal","math","modulefinder","posix","pyexpat","python_re","unicodedata","xml_helpers","xml_parser"];
 ;
 
